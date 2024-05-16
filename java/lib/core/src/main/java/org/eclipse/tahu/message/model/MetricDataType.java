7fbc33127f845eff8e2a575af29cd443c1113b66
Ilya Binshtok <ilya.binshtok@cirrus-link.com>
2024-05-16 15:24:34 -0500

    [IMM-4105] Added the hashCode() and equals() methods to the MetricDataType class.

--- java/lib/core/src/main/java/org/eclipse/tahu/message/model/MetricDataType.java
+++ java/lib/core/src/main/java/org/eclipse/tahu/message/model/MetricDataType.java
@@ -264,4 +264,27 @@ public class MetricDataType {
 	public String toString() {
 		return type;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + intValue;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		MetricDataType other = (MetricDataType) obj;
+		return intValue == other.intValue;
+	}
 }
