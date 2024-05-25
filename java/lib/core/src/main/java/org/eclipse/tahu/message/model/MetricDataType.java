615f5e153542767be8f843a54c5b47a1e0c99723
wes-johnson <wes0johnson@gmail.com>
2024-05-24 19:15:14 -0700

    Fixed duplicate hashCode and equals methods in MetricDataType

--- java/lib/core/src/main/java/org/eclipse/tahu/message/model/MetricDataType.java
+++ java/lib/core/src/main/java/org/eclipse/tahu/message/model/MetricDataType.java
@@ -297,12 +297,4 @@ public class MetricDataType {
 	public String toString() {
 		return type;
 	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + intValue;
-		return result;
-	}
 }
